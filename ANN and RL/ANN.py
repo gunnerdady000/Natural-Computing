e2e6d3296baf033509562de0aaf41c0d0069254d
Micah <56775725+gunnerdady000@users.noreply.github.com>
2021-03-19 13:01:04 -0600

    I give up
    I do not undersatnd, fix my GitHub no work

---
+++ ANN and RL/ANN.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+class ANN(object):
+
+    # Initialize class
+    def __init__(self):
+        self.sam_sucks = 0
---
+++ ANN and RL/test_ann.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def main():
+    print("For testing the ANN")
+
+if __name__ == "__main__":
+    main()
