52bf5c999c869a8e56e23dc182c006aa393bff10
Micah <56775725+gunnerdady000@users.noreply.github.com>
2021-03-23 19:27:51 -0600

    Updated ANN
    I got most of the function calls all setup, but I am still working on making the forward and backward prop be able to fit any amount of layers given by the user.
    
    I have the weights, Z's, A's, and b's all setup to work

--- ANN and RL/ANN.py
+++ ANN and RL/ANN.py
@@ -6,5 +6,123 @@ import time
 class ANN(object):
 
     # Initialize class
-    def __init__(self):
-        self.sam_sucks = 0
+    def __init__(self, inputs=2, outputs=2, layers=None, niter=10, learning_rate=0.1):
+        # create empty layer and checker
+        self.is_empty = False
+
+        if layers is None:
+            layers = np.array([])
+            self.is_empty = True
+
+        # holds number of inputs
+        self.inputs = inputs
+
+        # holds number of outputs
+        self.outputs = outputs
+
+        # ndarray of integers each value being the number of nodes in a hidden layer
+        self.layers = layers
+
+        # learning rate
+        self.learning_rate = learning_rate
+
+        # number of iterations
+        self.nither = niter
+
+    # Train the net by finding weights
+    def train(self, X, Y):
+        # get the input values
+        self.X = X
+        # get the output values
+        self.Y = Y
+
+    # Create weights based off of the layers ndarray
+    def weight_creation(self):
+        # array of arrays to hold the weights
+        self.weights = []
+
+        # loop is 1 to end of list
+        if not self.is_empty:
+            # first weight is inputs and layers[0]
+            weight_n = np.random.rand(self.inputs, self.layers[0])
+
+            # append to list
+            self.weights.append(weight_n)
+
+            # 1 until the one before the end of the array
+            for ind in range(1, self.layers.size):
+                # current list will n-1 and n
+                weight_n = np.random.rand(self.layers[ind-1], self.layers[ind])
+                self.weights.append(weight_n)
+
+            # last layer is n-layers by outputs
+            weight_n = np.random.rand(self.layers[self.layers.size - 1], self.outputs)
+            self.weights.append(weight_n)
+
+            # array of biases that correspond to their respective weights
+            self.b = np.random.rand(self.layers.size, 1)
+
+            # hold A's
+            self.A = np.zeros(self.layers.size - 1)
+
+            # hold the Z's
+            self.Z = np.zeros(self.layers.size)
+        else:
+            # only one layer results in a single weight input by output
+            self.weights = np.random.rand(self.inputs, self.outputs)
+
+            # only one bias point
+            self.b = np.random.rand(1)
+
+            # only one A
+            self.A = np.zeros(1)
+
+            # two Z's
+            self.Z = np.zeros(2)
+
+    # Predict using trained weights
+    def predict(self, X):
+        pass
+
+    # Forward Propagation
+    def forward_prop(self):
+        # start with n = 0
+        self.Z[0] = np.dot(self.X, self.weights[0]) + self.b[0]
+
+        # Zn+1 = X dot Wn+1 + bn+1
+        if not self.is_empty:
+            # A0 is relu of Z0
+            self.A[0] = self.relu(self.Z[0])
+            # Zn = An-1 dot W[n] + b[n]
+            Zn = np.dot(self.A[0], self.weights[1])
+
+            for n in range(1, self.layers.size):
+                Zn = self.X.dot(self.weights[n]) + self.b[n]
+                # relu
+
+        # end with n = size - 1
+
+    # Calculate summation
+    def summation(self, x, w, b):
+        # Zn = X dot Wn + bn
+        return np.dot(x, w) + b
+
+    # Calculate using sigmoid
+    def sigmoid(self, Z):
+        return 1.0 / (1 + np.exp(-Z))
+
+    # Derivative of sigmoid
+    def def_sigmoid(self, Z):
+        return self.sigmoid(Z) * (1 - self.sigmoid(Z))
+
+    # Calculate using relu
+    def relu(self, Z):
+        return np.maximum(0, Z)
+
+    # Derivative of the relu
+    def back_relu(self, x):
+        return np.where(x <= 0, 0, 1)
+
+    # Cost function
+    def cost(self, y, y_predict):
+        pass
--- ANN and RL/test_ann.py
+++ ANN and RL/test_ann.py
@@ -1,7 +1,40 @@
-import pandas as pd
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from ANN import ANN
 
 def main():
     print("For testing the ANN")
+    data = pd.read_csv("testdata.dat")
+    # put [0:1] into X array
+    x = data.iloc[0:38, [0, 1]].values
+    # change 1 or 2 into (0,1) or (1,0) into Y array
+    y = data.iloc[0:38, 2].values
+
+    # subtract down to 0 or 1
+    y = y - 1
+
+    # plot
+    #plot(x,y)
+
+    # one hot trick from https://stackoverflow.com/questions/29831489/convert-array-of-indices-to-1-hot-encoded-numpy-array
+    y = np.eye(np.max(y) + 1, dtype=int)[y]
+    layers = np.array([2,3,4])
+    net = ANN(2, 2, layers, 10, 0.1)
+    net.weight_creation()
+    net.weights
+    for i in range(1,3):
+        print(i)
+
+def plot(x, y):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    # create a np array
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=x[y == cl, 0], y=x[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
