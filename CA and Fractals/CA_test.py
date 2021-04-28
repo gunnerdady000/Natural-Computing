989dc7d2bdb5393c074295b27133c1bc26c4106f
Micah <56775725+gunnerdady000@users.noreply.github.com>
2021-04-28 12:29:54 -0600

    Updated testing
    McGough only wants us to start with 20 cells instead of 25.

--- CA and Fractals/CA_test.py
+++ CA and Fractals/CA_test.py
@@ -4,8 +4,8 @@ from CA import cellar_automa
 
 def main():
     # test with 25 cells and 125 iterations
-    cell = cellar_automa()
-    cell.laplace(True)
+    cell = cellar_automa(20, 125)
+    cell.laplace()
     cell.graph()
     # test with 125 cells and 1000 iterations
     cell = cellar_automa(125, 1000)
