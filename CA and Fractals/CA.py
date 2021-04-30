3c086ee01116730e1aa1cd3d06476ce71bdfaddb
DakotaSamuel <samuel.ryckman@mines.sdsmt.edu>
2021-04-30 06:58:05 -0600

    Make minor tweaks to CA code

--- CA and Fractals/CA.py
+++ CA and Fractals/CA.py
@@ -13,6 +13,8 @@ class cellar_automa(object):
         self.image = np.zeros((self.number_cells, self.number_cells))
         # used as a buffer image as everything changes
         self.buffer = np.zeros((self.number_cells, self.number_cells))
+        # Track change amounts across the grid
+        self.adjustments = np.zeros(niter)
 
     def laplace(self, images=False):
         # loop through and create initial conditions
@@ -25,6 +27,10 @@ class cellar_automa(object):
             plt.ion()
             plt.figure()
 
+        self.buffer = np.copy(self.image)
+
+        print("\rRunning CA: 0%", end="")
+
         # begin partial diff
         for t in range(self.niter):
             for i in range(1, self.number_cells - 1):
@@ -32,26 +38,40 @@ class cellar_automa(object):
                     self.buffer[i, j] = (self.image[i - 1, j] + self.image[i + 1, j] + self.image[i, j - 1] +
                                          self.image[i, j + 1]) / 4.0
 
+            # Calculate the average delta for the cells
+            self.adjustments[t] = np.abs(np.average(self.image - self.buffer))
+
             # move buffer state into image state
-            for i in range(1, self.number_cells - 1):
-                for j in range(1, self.number_cells - 1):
-                    self.image[i, j] = self.buffer[i, j]
+            self.image = np.copy(self.buffer)
 
             # maybe pictures
             if images:
                 self.graph(images)
+
+            print('\rRunning CA: {:.2f}%  '.format(t / self.niter * 100), end="")
+
+        print('\rRunning CA: 100%  ')
 
         # close multiple images
         if images:
             plt.ioff()
             plt.close()
 
-    def graph(self, images=False):
+    def graph(self, images=False, title="CA Results"):
         if images:
             plt.clf()
 
+        plt.title(title)
         plt.imshow(self.image, cmap=plt.cm.get_cmap("gray"), interpolation='nearest')
         if images:
             plt.pause(0.01)
         else:
             plt.show()
+
+    def graph_converge(self, title="Convergence"):
+        plt.clf()
+        plt.title(title)
+        plt.xlabel("Iteration")
+        plt.ylabel("Cell Change")
+        plt.plot(self.adjustments)
+        plt.show()
--- CA and Fractals/CA_test.py
+++ CA and Fractals/CA_test.py
@@ -3,14 +3,16 @@ from CA import cellar_automa
 
 
 def main():
-    # test with 25 cells and 125 iterations
-    cell = cellar_automa(20, 125)
+    # test with 25 cells and 150 iterations
+    cell = cellar_automa(20, 150)
     cell.laplace()
     cell.graph()
-    # test with 125 cells and 1000 iterations
-    cell = cellar_automa(125, 1000)
+    cell.graph_converge()
+    # test with 125 cells and 800 iterations
+    cell = cellar_automa(125, 800)
     cell.laplace()
     cell.graph()
+    cell.graph_converge()
 
 if __name__ == "__main__":
     main()
